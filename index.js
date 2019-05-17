ee0d0f9164e85968d1871787d5632f06ebea6acc
IsolatedTraveler <tbydmi@gmail.com>
2019-05-17 17:08:26 +0800

    20190517
    重构

--- index.js
+++
@@ -1,9 +0,0 @@
-(()=>{
-  'use strict';
-  const {checkCommand,commandExe} = require('./common.js')
-  const config = require('./config.js')
-  let params = process.argv.slice(2)
-  checkCommand(params,config).then(res=>{
-    commandExe(params,res,'./tools/')
-  })
-})()
